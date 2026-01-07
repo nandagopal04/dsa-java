35b177b88ac427ae2129267bac3ee534bfe4e95b
Nanda Gopal Ikkurthi <nanduikkurthi@gmail.com>
2026-01-07 15:18:19 +0530

    add prepend() in DoublyLinkedList

--- src/com/dsa/structure/DoublyLinkedList.java
+++ src/com/dsa/structure/DoublyLinkedList.java
@@ -90,4 +90,17 @@ public class DoublyLinkedList {
 		return temp;
 	}
 
+	public void prepend(int value) {
+		Node newNode = new Node(value);
+		if (length == 0) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			head.prev = newNode;
+			newNode.next = head;
+			head = newNode;
+		}
+		length++;
+	}
+
 }
